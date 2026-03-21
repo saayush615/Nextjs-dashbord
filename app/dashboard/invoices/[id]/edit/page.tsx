1cb5d3cafa3effcbc07dada93b8f61ac102ce0fa
Aayush singh <saayush615@gmail.com>
2026-03-21 10:38:11 +0530

    md12(end): handling 404 not found error

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,4 +1,5 @@
 import Form from "@/app/ui/invoices/edit-form";
+import { notFound } from "next/navigation";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 
@@ -10,6 +11,10 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    notFound();
+  }
   return (
     <main>
       <Breadcrumbs
